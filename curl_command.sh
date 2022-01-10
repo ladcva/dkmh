56bcb96febafb464cd7a9cc03089bdb4b85d4df5
Anh Duc Le <ladcva@yahoo.com>
2022-01-10 21:56:09 +0700

    Remove old chunks

---
+++ curl_command.sh
@@ -0,0 +1,18 @@
+curl 'https://sv.isvnu.vn/dang-ky-hoc-phan.html' \
+  -H 'authority: sv.isvnu.vn' \
+  -H 'sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"' \
+  -H 'accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
+  -H 'x-requested-with: XMLHttpRequest' \
+  -H 'sec-ch-ua-mobile: ?1' \
+  -H 'user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Mobile Safari/537.36' \
+  -H 'sec-ch-ua-platform: "Android"' \
+  -H 'origin: https://sv.isvnu.vn' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'referer: https://sv.isvnu.vn/dang-ky-hoc-phan.html' \
+  -H 'accept-language: en-US,en;q=0.9,vi;q=0.8' \
+  -H 'cookie: _ga=GA1.2.1494588641.1632883199; ASC.AUTH=CD2FCDA7BADA7CBE90EB5C26C5737A6E43FDF163BE613FD791F1ED00BF97E5A6E9C510466024B7DB78536AD8B933E0460040409C2C3B9E0357A714D69AB937AA0FCBDE886C91300CDAECDBEAF4AE908785CBC3C24E38AD96822101B06C4F310AD1000168D26951F62709BC05DC385D1BF37D4E0ECBD8DF3DD5B22CF37C994CC9; ASP.NET_SessionId=0srx0k41m22wjtzf4gxk3a0l; _gid=GA1.2.654659472.1641817287; _gat_gtag_UA_184858033_10=1' \
+  --data-raw 'param%5BIDDotDangKy%5D=27&param%5BIDLoaiDangKy%5D=2&param%5BGuidIDLopHocPhan%5D=BsJhpj7s8I-UoFclLq3MCw&param%5BIDNhomThucHanh%5D=' \
+  --compressed
