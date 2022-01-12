fd7f822d856370818cc9cd8618a718356539b54e
Anh Duc Le <ladcva@yahoo.com>
2022-01-12 23:17:10 +0700

    Change format log messages

--- request_script.py
+++ request_script.py
@@ -1,15 +1,20 @@
 import subprocess
 from time import sleep
+from datetime import datetime
+
 
 def cron_request():
     attempt = subprocess.check_output(['sh', 'curl_command.sh'], stderr=subprocess.PIPE, timeout=5.0)
+
+    now = datetime.now()
+    now = now.strftime("[%d/%m/%Y, %H:%M:%S]")
 
     # decode the bytes to string
     attempt = attempt.decode('utf-8')
 
     # print output of curl command to cron_request.log
     with open('cron_response.log', 'a') as f:
-        f.write(attempt)
+        f.write(now + ' ' + attempt)
         f.write('\n')
         f.close
 
