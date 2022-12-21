bd9419ce9233f0745919f545815e82722e5199d3
Anh Duc Le <ladcva@yahoo.com>
2022-12-21 19:33:59 +0700

    Initialize executor service

---
+++ executor/worker.py
@@ -0,0 +1,8 @@
+from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+from multiprocessing import Pool
+from random import random, randint, randrange
+from time import sleep
+from datetime import datetime
+
+class Worker:
+    pass
