e0e453d28dcd7b134a9f73699d0bdde5799443cc
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-03-12 15:02:11 +0700

    implement multiprocessing

--- crawler/test_1.py
+++ crawler/test_1.py
@@ -1,5 +1,7 @@
-import requests, itertools
+import requests, itertools, time
 from config.default import ASC_AUTH_STR
+from multiprocessing import Pool, cpu_count
+from functools import partial
 
 
 def get_all_subjects():
@@ -9,14 +11,14 @@ def get_all_subjects():
     codes = ([f"{i}{j}{k:03d}" for i, j, k in itertools.product(list1, list2, list3)])
     return codes
 
+
 def validate_subject_code(semester_id, subject_code: str):
     url = f"https://sv.isvnu.vn/SinhVienDangKy/LopHocPhanChoDangKy?IDDotDangKy={semester_id}&MaMonHoc={subject_code}&DSHocPhanDuocHoc={subject_code}&IsLHPKhongTrungLich=true&LoaiDKHP=1"  #test with a fixed IDDotdangky
     cookie = {'ASC.AUTH': ASC_AUTH_STR}
     
     response = requests.post(url, cookies=cookie)
 
-    if "lhpchodangky-notfound" in response.text:
-        print(f"{subject_code} does not exist") #testing
+    if "lhpchodangky-notfound" in response.text:    # Only return codes that are existed
         return None
     else:
         print(f"{subject_code} exist") #fetch the code to database
@@ -24,16 +26,23 @@ def validate_subject_code(semester_id, subject_code: str):
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     codes_list = get_all_subjects()
     available_subject_codes = []
+    num_processes = cpu_count()  # Get number of CPU cores available
+    chunk_size = len(codes_list) // num_processes  # Determine chunk size for each process
 
-    for subject_code in codes_list:
-        exist_current_sbj = validate_subject_code(35, subject_code)
-        if exist_current_sbj:
-            available_subject_codes.append(exist_current_sbj)
+    with Pool(processes=num_processes) as pool:
+        func = partial(validate_subject_code, 35)   # Create a partial func to pass semester_id to validate_subject_code
+        for result in pool.imap_unordered(func, 35, codes_list, chunksize=chunk_size): # Dont need ordered results, so imap_unordered will gain performance
+            if result:
+                available_subject_codes.append(result)
 
     print(available_subject_codes)
+    end_time = time.time()
+    print(f"Processing time: {end_time - start_time} seconds")
 
-#TODO Refactor and use multiprocessing to speed up the process or think of a better way to optimize the process
+    
+#TODO Refactor and use multiprocessing to speed up the process or think of a better way to optimize the process -> partially done, multiprocessing implemented
 #TODO Use a database to store the results
 #TODO Get all semester IDs and run the process for each semester ID
