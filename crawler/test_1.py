2b1d28d59f5b2dbe7ad0c3e856ecac0def5d5189
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-03-19 12:26:27 +0700

    fix variable name

--- crawler/test_1.py
+++ crawler/test_1.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     for ID in IDs: # For id in semester ids
         with Pool(processes=num_processes) as pool:
             func = partial(validate_subject_code, ID)   # Create a partial func to pass semester_id to validate_subject_code
-            for result in pool.imap_unordered(func, codes_list, chunksize=chunk_size): # Dont need ordered results, so imap_unordered will gain performance
+            for result in pool.imap_unordered(func, codes_list, chunksize=chunk_size): # Don't need ordered results, so imap_unordered will gain performance
                 if result:
                     available_subject_codes.append(result)
 
