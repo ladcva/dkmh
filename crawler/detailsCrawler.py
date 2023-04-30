83ced8b2a3db1a00593d00dfb8d1b3232e0d05cd
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-04-30 16:48:31 +0700

    redesign database schema

--- crawler/detailsCrawler.py
+++ crawler/detailsCrawler.py
@@ -55,7 +55,7 @@ def crawl_lhp_data(code):
 
 if __name__ == "__main__":
     temp_instance = TempLists()
-    lastest_sem_id = get_semester_id()[1] # index 1 is for testing purpose, the lastest sem ID doesn't have any codes yet
+    latest_sem_id = get_semester_id()[0] # index 1 is for testing purpose, the latest sem ID doesn't have any codes yet
     class_codes = [item[0] for item in get_class_codes()]
     chunk_size = len(class_codes) // DEFAULT_NUM_PROCESSES
 
