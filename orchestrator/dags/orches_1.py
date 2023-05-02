f2a057942861b814eb0ee5fc04c3bcf023f46a96
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-05-02 16:25:00 +0700

    doing DAG

--- orchestrator/dags/orches_1.py
+++ orchestrator/dags/orches_1.py
@@ -7,18 +7,40 @@ with DAG(
     default_args={
         "owner": "Binh",
         "retries" :1,
-        "retry_delay": dates.timedelta(minutes=1),
-
+        "retry_delay": dates.timedelta(minutes=5),
+        "xcom_push": True,
     },
     description="Test DAG",
-    schedule=dates.timedelta(days=1),
+    schedule='@daily',
     start_date=dates.days_ago(2),
     tags=["test"],
 
 )as dag:
-    t1 = BashOperator(
-        task_id="test_orches_1",
-        bash_command="cd /opt/airflow/dkmh && python3 test.py",
+    initialize_env = BashOperator(
+        task_id="activate_environment",
+        bash_command="cd /opt/airflow/dkmh && source venv/bin/activate",  # Maybe for now we should install all dependencies beforehand and then use this DAG
     )
 
+    create_db = BashOperator(
+        task_id="create_database",
+        bash_command = "python -m db_migration.create_db",
+    )
 
+    cdc = BashOperator(
+        task_id="CDC_for_semester",
+        bash_command = "python -m crawler.CDC",
+    )
+
+    crawl_classes = BashOperator(
+        task_id="crawl_all_classes",
+        bash_command = "python -m crawler.classesCrawler",
+    )
+
+    crawl_class_details = BashOperator(
+        task_id="crawl_details_for_all_classes",
+        bash_command = "python -m crawler.detailsCrawler",
+    )
+
+    initialize_env >> create_db >> cdc >> crawl_classes >> crawl_class_details
+
+# TODO: use XCOM to add conditions to the DAG
