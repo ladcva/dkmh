b32cbc6949bab58abf05aaa748eedc62554b1b96
Anh Duc Le <ladcva@yahoo.com>
2023-05-11 22:21:19 +0700

    Fix bug docker-compose and postgres config

--- curl_command.sh
+++ curl_command.sh
@@ -1,18 +1,18 @@
 curl 'https://sv.isvnu.vn/SinhVienDangKy/MonHocPhanChoDangKy' \
-  -H 'authority: sv.isvnu.vn' \
-  -H 'accept: text/html, */*; q=0.01' \
-  -H 'accept-language: en-US,en;q=0.9,vi;q=0.8' \
-  -H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
-  -H 'cookie: ASP.NET_SessionId=eegxrqrbdm5xxp2cxrnql3hh; __RequestVerificationToken=72wUCejJDOAf2mifGTwY-ui2wdHPQ-D8q1egJi0BRoGloRQLlM4SwS-JEfkDAuo39egNoDsvkgU1w5qa-hjoKjnCKm9vuLxSPDI_-lUNssE1; SinhVien_LangName=; _ga=GA1.2.343368229.1671026498; ASC.AUTH=34552BB4081FAC768BE5324FDA464E5EB259E619364F4EBA217AE33D00D1326B6960FD0E5780AE22A99F327D358B272212E212DCB364BCCD089CE31AB0B46F7F5319713E122E7BE5EDD81C169507EA03C77EAE9D1884D4ECEBC07FCEF7AD2256C350BF58BCCC148C48DB8EB5CDE38CA306D6C5EE6A63927270488275FE515377' \
-  -H 'origin: https://sv.isvnu.vn' \
-  -H 'referer: https://sv.isvnu.vn/dang-ky-hoc-phan.html' \
-  -H 'sec-ch-ua: "Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108"' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "Windows"' \
-  -H 'sec-fetch-dest: empty' \
-  -H 'sec-fetch-mode: cors' \
-  -H 'sec-fetch-site: same-origin' \
-  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46' \
-  -H 'x-requested-with: XMLHttpRequest' \
+  -J 'authority: sv.isvnu.vn' \
+  -J 'accept: text/html, */*; q=0.01' \
+  -J 'accept-language: en-US,en;q=0.9,vi;q=0.8' \
+  -J 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
+  -J 'cookie: ASP.NET_SessionId=eegxrqrbdm5xxp2cxrnql3hh; __RequestVerificationToken=72wUCejJDOAf2mifGTwY-ui2wdHPQ-D8q1egJi0BRoGloRQLlM4SwS-JEfkDAuo39egNoDsvkgU1w5qa-hjoKjnCKm9vuLxSPDI_-lUNssE1; SinhVien_LangName=; _ga=GA1.2.343368229.1671026498; ASC.AUTH=34552BB4081FAC768BE5324FDA464E5EB259E619364F4EBA217AE33D00D1326B6960FD0E5780AE22A99F327D358B272212E212DCB364BCCD089CE31AB0B46F7F5319713E122E7BE5EDD81C169507EA03C77EAE9D1884D4ECEBC07FCEF7AD2256C350BF58BCCC148C48DB8EB5CDE38CA306D6C5EE6A63927270488275FE515377' \
+  -J 'origin: https://sv.isvnu.vn' \
+  -J 'referer: https://sv.isvnu.vn/dang-ky-hoc-phan.html' \
+  -J 'sec-ch-ua: "Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108"' \
+  -J 'sec-ch-ua-mobile: ?0' \
+  -J 'sec-ch-ua-platform: "Windows"' \
+  -J 'sec-fetch-dest: empty' \
+  -J 'sec-fetch-mode: cors' \
+  -J 'sec-fetch-site: same-origin' \
+  -J 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46' \
+  -J 'x-requested-with: XMLHttpRequest' \
   --data-raw 'param%5BIDDotDangKy%5D=35&param%5BIDLoaiDangKy%5D=1' \
   --compressed
