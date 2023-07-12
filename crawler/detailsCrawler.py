4337c4546fe9b5fb577244847ec07d659d31298a
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-07-12 23:14:12 +0700

    detailsCrawler refactoring

--- crawler/detailsCrawler.py
+++ crawler/detailsCrawler.py
@@ -5,7 +5,7 @@ from utils.utils import get_semester_id, get_class_codes, insert_to_latest_sem, 
 from config.default import ASC_AUTH_STR, HP_URL, LH_URL, DEFAULT_NUM_PROCESSES
 
 def crawl_lhp_data(code):
-    temp, temp2, temp3 = [], [], []
+    class_info_list, class_schedule_list = [], []
     cookie = {'ASC.AUTH': ASC_AUTH_STR}
     url = HP_URL.format(latest_sem_id, code, code)
     response = requests.post(url, cookies=cookie)
@@ -27,11 +27,11 @@ def crawl_lhp_data(code):
             subject_code = span_element.next_sibling.strip().split(' - ')[1]
             subject_name = soup.find_all('div', class_='name')[0].text
             print(guid, '-', class_code, '-',subject_code, '-',subject_name)
-            temp.append((guid, class_code, subject_code, subject_name))
+            class_info_list.append((guid, class_code, subject_code, subject_name))
         except IndexError:
             break
 
-    for item in temp:
+    for item in class_info_list:
         guid = item[0]
         response2 = requests.post(LH_URL, cookies=cookie, data={'GuidIDLopHocPhan': guid})
         soup2 = BeautifulSoup(response2.text, 'html.parser')
@@ -40,34 +40,50 @@ def crawl_lhp_data(code):
         room = tag[3].text
         lecturer = tag[6].text
         timeframe = tag[7].text
-        temp2.append((schedule, room, lecturer, timeframe))
-    temp3 = [x + y for x, y in zip(temp, temp2)]
-    return(temp3)
+        class_schedule_list.append((schedule, room, lecturer, timeframe))
+    combined_list = [x + y for x, y in zip(class_info_list, class_schedule_list)]
+    return(combined_list)
 
 if __name__ == "__main__":
     start_time = time.time()
     
+    # Initialize variables
     temp_instance = TempLists()
     latest_sem_id = get_semester_id()[0] # 0 = newest semester
     class_codes = [item[0] for item in get_class_codes()]
     num_processes = DEFAULT_NUM_PROCESSES*3        # *3
     chunk_size = len(class_codes) // num_processes  # Determine chunk size for each process
 
+    # Crawl data
     with Pool(processes=num_processes) as p:
         results = p.map(crawl_lhp_data, class_codes, chunksize=chunk_size)
     for result in results:
         temp_instance.add_data(result)
     time.sleep(1)
     
+    # Insert data into database
+    print("Inserting data into classes...")
     insert_to_classes(temp_instance.subject_codes)
-    insert_to_latest_sem(guids=temp_instance.guids, subject_codes=temp_instance.subject_codes, subject_names=temp_instance.subject_names, course_codes=temp_instance.course_codes,
-                         semester_id=latest_sem_id, schedules=temp_instance.schedules, rooms=temp_instance.rooms, lecturers=temp_instance.lecturers, timeframes=temp_instance.timeframes)
+    print("Data inserted into classes.")
+
+    print("Inserting data into latest semester...")
+    insert_to_latest_sem(
+        guids=temp_instance.guids, 
+        subject_codes=temp_instance.subject_codes, 
+        subject_names=temp_instance.subject_names, 
+        course_codes=temp_instance.course_codes,
+        semester_id=latest_sem_id, 
+        schedules=temp_instance.schedules, 
+        rooms=temp_instance.rooms, 
+        lecturers=temp_instance.lecturers, 
+        timeframes=temp_instance.timeframes
+)
+    print("Data inserted into latest semester.")
     
+    # Results
     end_time = time.time()
-    
     print('The number of classes scheduled is:',len(temp_instance.guids))
     print(f"Processing time: {end_time - start_time - 1} seconds")
-    
     if len(temp_instance.guids) > 0:
         print("Task completed")
     else:
