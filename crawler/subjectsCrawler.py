6d65df0e1a48ec6bd766c9bff1be5b8db7cc0e7a
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-08-13 09:05:51 +0700

    add more logic to subjectsCrawler

--- crawler/subjectsCrawler.py
+++ crawler/subjectsCrawler.py
@@ -1,7 +1,7 @@
 import requests, itertools, time
 from config.default import ASC_AUTH_STR, DEFAULT_NUM_PROCESSES, PROCESSES_FACTOR, HP_URL
 from multiprocessing import Pool
-from utils.utils import get_semester_id, insert_latest_id
+from utils.utils import get_semester_id, insert_latest_id, diff_with_penultimate_semester_snapshot
 from functools import partial
 
 semester_ids = get_semester_id()
@@ -40,13 +40,21 @@ if __name__ == "__main__":
     codes_list = get_all_subjects()
     num_processes = DEFAULT_NUM_PROCESSES * PROCESSES_FACTOR  
     chunk_size = len(codes_list) // num_processes  
-
-    for semester in semester_ids:
+    diff_sem = diff_with_penultimate_semester_snapshot()
+    
+    if diff_sem:
         with Pool(processes=num_processes) as pool:
-            func = partial(validate_subject_code, semester)   # Only crawl all the semesters for 1st run
+            func = partial(validate_subject_code, diff_sem)   # Crawl the lastest semester
             for result in pool.imap_unordered(func, codes_list, chunksize=chunk_size): # Don't need ordered results, so imap_unordered will gain performance
                 if result:
                     available_subject_codes.append(result)
+    else:
+        for semester in semester_ids:
+            with Pool(processes=num_processes) as pool:
+                func = partial(validate_subject_code, semester)   # Crawl all of the semesters
+                for result in pool.imap_unordered(func, codes_list, chunksize=chunk_size): # Don't need ordered results, so imap_unordered will gain performance
+                    if result:
+                        available_subject_codes.append(result)
 
     print(available_subject_codes)
 
